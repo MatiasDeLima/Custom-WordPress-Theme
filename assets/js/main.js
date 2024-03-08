26db076ef963408542c1ac898fba7351f3c13218
Lucas Matias <101410443+MatiasDeLima@users.noreply.github.com>
2024-03-08 16:44:17 -0300

    feat create button animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,3 +73,31 @@ sr.reveal(`.swiper, .analize__data, .demo__data, .advantages__image, .contact__i
 //     el: document.querySelector('#main'),
 //     smooth: true
 // });
+
+/*=============== BUTTON HOVER ===============*/
+let elements = document.querySelectorAll(".text");
+
+elements.forEach((element) => {
+    let innerText = element.innerText;
+    console.log(innerText)
+    element.innerHTML = "";
+
+    let textContainer = document.createElement("div");
+    textContainer.classList.add("block")
+
+    for (let letter of innerText) {
+        let span = document.createElement("span");
+        span.innerText = letter.trim() === "" ? "\xa0" : letter;
+        span.classList.add("letter");
+        textContainer.appendChild(span);
+    }
+
+    element.appendChild(textContainer);
+    element.appendChild(textContainer.cloneNode(true));
+})
+
+elements.forEach((element) => {
+    element.addEventListener("mouseover", () => {
+        element.classList.remove("play")
+    })
+})
